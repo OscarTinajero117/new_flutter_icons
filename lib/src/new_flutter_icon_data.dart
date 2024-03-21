a242b62dc1d5414bcaf96da5e974ec4998d9b8db
OscarTinajero117 <tinajero117.oscar@gmail.com>
2024-03-21 17:23:53 -0600

    Update Icons

--- lib/src/new_flutter_icon_data.dart
+++ lib/src/new_flutter_icon_data.dart
@@ -5,23 +5,8 @@ class NewFlutterIconData extends IconData {
   const NewFlutterIconData(super.codePoint, String fontFamily)
       : super(fontFamily: fontFamily, fontPackage: "new_flutter_icons");
 
-  const NewFlutterIconData.ionicons(int codePoint)
-      : this(codePoint, "Ionicons");
-
   const NewFlutterIconData.antDesign(int codePoint)
       : this(codePoint, "AntDesign");
-
-  const NewFlutterIconData.fontAwesome(int codePoint)
-      : this(codePoint, "FontAwesome");
-
-  const NewFlutterIconData.fontAwesome5Brands(int codePoint)
-      : this(codePoint, "FontAwesome5_Brands");
-
-  const NewFlutterIconData.fontAwesome5(int codePoint)
-      : this(codePoint, "FontAwesome5");
-
-  const NewFlutterIconData.fontAwesome5Solid(int codePoint)
-      : this(codePoint, "FontAwesome5_Solid");
 
   const NewFlutterIconData.entypo(int codePoint) : this(codePoint, "Entypo");
 
@@ -32,6 +17,33 @@ class NewFlutterIconData extends IconData {
 
   const NewFlutterIconData.foundation(int codePoint)
       : this(codePoint, "Foundation");
+
+  const NewFlutterIconData.fontAwesome(int codePoint)
+      : this(codePoint, "FontAwesome");
+
+  const NewFlutterIconData.fontAwesome5Brands(int codePoint)
+      : this(codePoint, "FontAwesome5_Brands");
+
+  const NewFlutterIconData.fontAwesome5Solid(int codePoint)
+      : this(codePoint, "FontAwesome5_Solid");
+
+  const NewFlutterIconData.fontAwesome5(int codePoint)
+      : this(codePoint, "FontAwesome5");
+
+  const NewFlutterIconData.fontAwesome6Brands(int codePoint)
+      : this(codePoint, "FontAwesome6_Brands");
+
+  const NewFlutterIconData.fontAwesome6Solid(int codePoint)
+      : this(codePoint, "FontAwesome6_Solid");
+
+  const NewFlutterIconData.fontAwesome6(int codePoint)
+      : this(codePoint, "FontAwesome6");
+
+  const NewFlutterIconData.fontisto(int codePoint)
+      : this(codePoint, "Fontisto");
+
+  const NewFlutterIconData.ionicons(int codePoint)
+      : this(codePoint, "Ionicons");
 
   const NewFlutterIconData.materialCommunityIcons(int codePoint)
       : this(codePoint, "MaterialCommunityIcons");
@@ -45,8 +57,8 @@ class NewFlutterIconData extends IconData {
   const NewFlutterIconData.simpleLineIcons(int codePoint)
       : this(codePoint, "SimpleLineIcons");
 
-  const NewFlutterIconData.zocial(int codePoint) : this(codePoint, "Zocial");
-
   const NewFlutterIconData.weatherIcons(int codePoint)
       : this(codePoint, "WeatherIcons");
+
+  const NewFlutterIconData.zocial(int codePoint) : this(codePoint, "Zocial");
 }
